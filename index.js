b592531d9dcb457f9c1529ea19482bfcbf9d8262
Pabloso8 <44539211+Pabloso8@users.noreply.github.com>
2021-11-17 23:37:30 -0500

    inventarios2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const PORT =  5001
+const PORT =  process.env.PORT || 5001
 
 app.use(express.json())
 
@@ -12,18 +12,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.static('build'));
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "http://192.168.0.106:8080"); 
-  // res.header("Access-Control-Allow-Origin", "http://www.datazo.info");
-  // res.header("Access-Control-Allow-Origin", "http://datazo.info");
-  // res.header("Access-Control-Allow-Origin", "https://datazo.herokuapp.com");
   res.header("Access-Control-Allow-Origin", "http://localhost:5000"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header("Access-Control-Allow-Origin", "https://datazo-workernode-3.herokuapp.com");
-  // res.header("Access-Control-Allow-Origin", "http://192.168.0.106:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
-});
+}); 
 
 // Prueba
 app.get('/hola', (req, res) => {
